e2b3b73135aa32c4b7b905541a55e4455c677b60
Sven <sven.vander.mierde@gmail.com>
2021-10-01 08:13:50 +0200

    randomized the intervals and cleaned up the code a little

--- optional/01-misc/05-worst-ui-three/script.js
+++ optional/01-misc/05-worst-ui-three/script.js
@@ -12,6 +12,8 @@
 (function() {
 
     // your code here
+
+    /*FUNCTIONS*/
     //write to target
     function writeToTarget() {
         document.getElementById("target").innerText = "0"
@@ -20,17 +22,25 @@
             + document.getElementById("part-three").value
             + document.getElementById("part-four").value;
     }
+    //Stop the slots
 
-//the prefix 04XX .. .. ..
+    function stop1() {
+        clearInterval(speed1);
+    }
 
-    let minValue1 = Number(document.getElementById("part-one").getAttribute("data-min"));
-    let maxValue1 = Number(document.getElementById("part-one").getAttribute("data-max"));
-    let currentValue1 = minValue1;
-    let speed1 = setInterval(slots1, 100);
+    function stop2() {
+        clearInterval(speed2);
+    }
 
-    document.getElementById("fix-part-one").addEventListener("click", function () {
-        stop1();
-    });
+    function stop3() {
+        clearInterval(speed3);
+    }
+
+    function stop4() {
+        clearInterval(speed4);
+    }
+
+    //the prefix 04XX .. .. ..
 
     function slots1() {
         if (currentValue1 <= maxValue1) {
@@ -43,20 +53,7 @@
         writeToTarget();
     }
 
-    function stop1() {
-        clearInterval(speed1);
-    }
-
-//digits 04.. XX .. ..
-
-    let minValue2 = Number(document.getElementById("part-two").getAttribute("data-min"));
-    let maxValue2 = Number(document.getElementById("part-two").getAttribute("data-max"));
-    let currentValue2 = minValue2;
-    let speed2 = setInterval(slots2, 100);
-
-    document.getElementById("fix-part-two").addEventListener("click", function () {
-        stop2();
-    });
+    //digits 04.. XX .. ..
 
     function slots2() {
         if (currentValue2 <= maxValue2) {
@@ -69,20 +66,7 @@
         writeToTarget();
     }
 
-    function stop2() {
-        clearInterval(speed2);
-    }
-
-//digits 04.. .. XX ..
-
-    let minValue3 = Number(document.getElementById("part-three").getAttribute("data-min"));
-    let maxValue3 = Number(document.getElementById("part-three").getAttribute("data-max"));
-    let currentValue3 = minValue3;
-    let speed3 = setInterval(slots3, 100);
-
-    document.getElementById("fix-part-three").addEventListener("click", function () {
-        stop3();
-    });
+    //digits 04.. .. XX ..
 
     function slots3() {
         if (currentValue3<= maxValue3) {
@@ -95,20 +79,7 @@
         writeToTarget();
     }
 
-    function stop3() {
-        clearInterval(speed3);
-    }
-
-//digits 04.. .. .. XX
-
-    let minValue4 = Number(document.getElementById("part-four").getAttribute("data-min"));
-    let maxValue4 = Number(document.getElementById("part-four").getAttribute("data-max"));
-    let currentValue4 = minValue4;
-    let speed4 = setInterval(slots4, 100);
-
-    document.getElementById("fix-part-four").addEventListener("click", function () {
-        stop4();
-    });
+    //digits 04.. .. .. XX
 
     function slots4() {
         if (currentValue4<= maxValue4) {
@@ -121,8 +92,54 @@
         writeToTarget();
     }
 
-    function stop4() {
-        clearInterval(speed4);
-    }
+    /*VARIABLES*/
+    //declaring min, max and current values. Declaring speed values for slots function
+
+    let minValue1 = Number(document.getElementById("part-one").getAttribute("data-min"));
+    let minValue2 = Number(document.getElementById("part-two").getAttribute("data-min"));
+    let minValue3 = Number(document.getElementById("part-three").getAttribute("data-min"));
+    let minValue4 = Number(document.getElementById("part-four").getAttribute("data-min"));
+
+    let maxValue1 = Number(document.getElementById("part-one").getAttribute("data-max"));
+    let maxValue2 = Number(document.getElementById("part-two").getAttribute("data-max"));
+    let maxValue3 = Number(document.getElementById("part-three").getAttribute("data-max"));
+    let maxValue4 = Number(document.getElementById("part-four").getAttribute("data-max"));
+
+    let currentValue1 = minValue1;
+    let currentValue2 = minValue2;
+    let currentValue3 = minValue3;
+    let currentValue4 = minValue4;
+
+    //just some fun with random intervals
+
+    let interval1 = Math.floor(Math.random() * 100) + 50;
+    let interval2 = Math.floor(Math.random() * 100) + 50;
+    let interval3 = Math.floor(Math.random() * 100) + 50;
+    let interval4 = Math.floor(Math.random() * 100) + 50;
+
+    let speed1 = setInterval(slots1, interval1);
+    let speed2 = setInterval(slots2, interval2);
+    let speed3 = setInterval(slots3, interval3);
+    let speed4 = setInterval(slots4, interval4);
+
+    /*BUTTONS*/
+    //adding eventListeners to the buttons
+
+    document.getElementById("fix-part-one").addEventListener("click", function () {
+        stop1();
+    });
+
+    document.getElementById("fix-part-two").addEventListener("click", function () {
+        stop2();
+    });
+
+
+    document.getElementById("fix-part-three").addEventListener("click", function () {
+        stop3();
+    });
+
+    document.getElementById("fix-part-four").addEventListener("click", function () {
+        stop4();
+    });
 
 })()
